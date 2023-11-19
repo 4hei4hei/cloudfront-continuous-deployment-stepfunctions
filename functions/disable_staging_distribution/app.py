54825976d1276ace5cc8d7a432fa62672d98b1ee
4hei4hei <4hei4hei@gmail.com>
2023-11-19 09:05:58 +0900

    add: disable_staging_distribution 関数を追加

---
+++ functions/disable_staging_distribution/app.py
@@ -0,0 +1,40 @@
+import boto3
+from botocore.config import Config
+
+
+config = Config(retries={"max_attempts": 10, "mode": "standard"})
+cloudfront = boto3.client("cloudfront", config=config)
+
+
+def lambda_handler(event, context):
+    payload = event["Payload"]
+    staging_distribution_id = payload["StagingDistributionId"]
+    staging_distribution_etag = cloudfront.get_distribution_config(
+        Id=staging_distribution_id
+    )["ETag"]
+    staging_distribution_config = cloudfront.get_distribution_config(
+        Id=staging_distribution_id
+    )["DistributionConfig"]
+
+    continuous_deployment_policy_id = payload["ContinuousDeploymentPolicyId"]
+    continuous_deployment_policy_etag = (
+        cloudfront.get_continuous_deployment_policy_config(
+            Id=continuous_deployment_policy_id
+        )["ETag"]
+    )
+
+    # Continuous Deployment Policy を削除する
+    cloudfront.delete_continuous_deployment_policy(
+        Id=continuous_deployment_policy_id, IfMatch=continuous_deployment_policy_etag
+    )
+
+    # Staging Distribution を Disabled にし、ContinuousDeploymentPolicy の関連付けを削除する
+    staging_distribution_config["Enabled"] = False
+    staging_distribution_config["ContinuousDeploymentPolicyId"] = ""
+    cloudfront.update_distribution(
+        Id=staging_distribution_id,
+        IfMatch=staging_distribution_etag,
+        DistributionConfig=staging_distribution_config,
+    )
+
+    return {"Payload": payload}
